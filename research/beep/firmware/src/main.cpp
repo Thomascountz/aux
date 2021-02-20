885af50eb8e4e5776d1e4752d92bd54f3a49f00e
Thomas Countz <thomascountz@gmail.com>
2021-02-20 14:12:32 -0500

    begin prototyping firmware for ESP32

---
+++ research/beep/firmware/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <pitches.h>
+
+const int buzzerPin = 0;
+const int buttonPin = 27;
+const int channel = 0;
+void gestureHello();
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  bool buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW) {
+    gestureHello();
+  }
+}
+ 
+ void gestureHello() {
+   Serial.print("triggered");
+   ledcAttachPin(buzzerPin, channel);
+   ledcWriteTone(channel, NOTE_C6);
+   delay(100);
+   ledcWriteTone(channel, NOTE_D6);
+   delay(100);
+   ledcWriteTone(channel, NOTE_G6);
+   delay(100);
+   ledcWriteTone(channel, NOTE_E6);
+   delay(100);
+   ledcDetachPin(buzzerPin);
+   ledcWrite(channel, 0);
+ }
